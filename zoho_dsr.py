2a76797671e2a053f744ece27c5b9326ffec9923
Shubham Kurkute <shubham.kurkute@cctech.co.in>
2025-07-14 17:06:39 +0530

    Added MCP tools for Fetching Data from git commit and update the DSR,

---
+++ zoho_dsr.py
@@ -0,0 +1,76 @@
+import http.client
+import json
+import datetime
+import subprocess
+import urllib.parse
+
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(description="Zoho DSR Tool to add DSR records in Zoho CRM")
+
+
+@mcp.tool()
+def create_dsr_record(activities:list[str]):
+    """
+    Create a Daily Work Status Report record in Zoho People.
+    Parameters:
+    - activities: List of activities to be recorded in the DSR.
+    Example:
+        create_dsr_record(["Activity 1", "Activity 2", "Activity 3"])
+    """
+    headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Authorization": f"Zoho-oauthtoken {get_access_token()}",
+        "Content-Type": "application/x-www-form-urlencoded" 
+    }
+
+    input_data = {
+        "inputData": { 
+            "DSR_Date": datetime.date.today().strftime("%d-%b-%Y"),
+            "Activity": activities,
+            "Lookup_1": "29929000007049341"
+        }
+    }
+    payload = urllib.parse.urlencode(input_data)
+
+    conn = http.client.HTTPSConnection("people.zoho.in")
+    conn.request("POST", "/api/forms/json/DSR/insertRecord", payload, headersList)
+    response = conn.getresponse()
+    result = response.read()
+
+    print(result.decode("utf-8"))
+
+
+def get_access_token():
+    """
+    Get access token from Zoho using OAuth2.
+    """
+
+    headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Content-Type": "application/x-www-form-urlencoded" 
+    }
+
+    # payload = "grant_type=authorization_code&client_id=1000.WFK56S2AP7E1DCH4HJPFBG7LKVUGSW&client_secret=193df6efed9cd734792830516b9080fef4e333e758&redirect_uri=http://localhost&code=1000.f8f7dd23f22e908048be43fd4e2de8bf.00dbad33e060826bb3e5e667cdd1ca70"
+    payload = ""
+    conn = http.client.HTTPSConnection("accounts.zoho.in")
+    conn.request("POST", "/oauth/v2/token?refresh_token=1000.ac22f5753323d784b7758db5756c5f54.24bae40731b3a407741b025a2c524091&client_id=1000.WFK56S2AP7E1DCH4HJPFBG7LKVUGSW&client_secret=193df6efed9cd734792830516b9080fef4e333e758&grant_type=refresh_token", payload, headersList)
+    response = conn.getresponse()
+    result = response.read()
+    response_data = json.loads(result.decode("utf-8"))
+    access_token = response_data.get("access_token")
+    return access_token
+
+
+@mcp.tool()
+def get_git_status_and_log():
+    """Run 'git status' and 'git log' commands and return their outputs as strings."""
+    status = subprocess.check_output(["git", "status"], text=True)
+    return status + "\n\n" + status
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
+    # create_dsr_record(["Activity 1", "Activity 2", "Activity 3"])
